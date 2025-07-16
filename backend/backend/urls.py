044c513f299e875f18c5fe0fc7eebc7b13b62f6d
Antony <toni4pius@gmail.com>
2025-07-16 18:15:57 +0300

    Add simple homepage view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🎉 Django is Live on Render!")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),  # This now includes all: djoser + properties
